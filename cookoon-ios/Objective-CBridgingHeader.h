93a09949977751bbc5d5a2d318d7aced6eb5590d
Charles Pernet <charlespernet@gmail.com>
2017-12-20 12:59:44 +0100

    Config for beta and store builds

---
+++ cookoon-ios/Objective-CBridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  Objective-CBridgingHeader.h
+//  cookoon-ios
+//
+//  Created by Charles PERNET on 20/12/2017.
+//  Copyright © 2017 Charles PERNET. All rights reserved.
+//
+
+#ifndef Objective_CBridgingHeader_h
+#define Objective_CBridgingHeader_h
+
+
+#endif /* Objective_CBridgingHeader_h */
